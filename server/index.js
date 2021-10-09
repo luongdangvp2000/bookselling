64d6be52ef70a9ea732b526d7d897682d0e5488d
luongdangvp2000 <luongdangvp2000@gmail.com>
2021-10-09 11:00:41 +0700

    design frontend

--- server/index.js
+++ server/index.js
@@ -3,22 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app= express();
 
-const app = express();
+app.get('/' , (req , res)=>{
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+   res.send('hello from simple server :)')
 
-const CONNECTION_URL = 'mongodb+srv://luongdangvp2000:Luongdangvp2000@cluster0.enzur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+});
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
-
-//mongoose.set('useFindAndModify', false);
-
-
+app.listen(5000, ()=>{
+    console.log('Serve at http://localhost:5000');
+});
 
 
